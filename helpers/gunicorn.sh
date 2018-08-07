62a5115f31e71bc989c7bfc38a1bb8acc6fded8b
Wes Young <wes@barely3am.com>
2018-08-07 16:38:05 -0400

    dep cleanup

--- helpers/gunicorn.sh
+++ helpers/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --worker-class gevent --log-level DEBUG cif.httpd.app:app -b localhost:5000 -k flask_sockets.worker -w 2 --graceful-timeout 15
+gunicorn --worker-class gevent --log-level DEBUG cif.httpd.app:app -b :5000 -k flask_sockets.worker -w 2 --graceful-timeout 15
