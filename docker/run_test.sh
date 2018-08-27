6ca9acdb0ba84d1530247b1706f24a278795311f
Wes Young <wes@barely3am.com>
2018-08-27 16:19:53 -0400

    cleaning up docker

--- docker/run_test.sh
+++ docker/run_test.sh
@@ -7,6 +7,14 @@ echo ""
 echo "${CIF_TOKEN}"
 echo ""
 
-C=$(docker run -e CIF_DOCKER_TEST=1 -e CIF_TOKEN="${CIF_TOKEN}" -e CIF_HUNTER_ADVANCED=1 -e CIF_HUNTER_THREADS=2 -e CIF_HUNTER_TRACE=1 -it -d -p 5000:5000 --name verbose-robot --memory 2g --memory-swap 4g csirtgadgets/verbose-robot)
+C=$(docker run -e CIF_TOKEN="${CIF_TOKEN}" -e CIF_HUNTER_ADVANCED=1 -e CIF_HUNTER_THREADS=2 -e CIF_HUNTER_TRACE=1 -it -d -p 5000:5000 --name verbose-robot --memory 2g --memory-swap 4g csirtgadgets/verbose-robot)
 
-docker exec -it $C /home/cif/test.sh
+bash helpers/test.sh
+
+if [ -z ${NO_AUTO_REMOVE} ]; then
+    echo 'shutting down in 90s'
+    sleep 90
+    docker rm -f verbose-robot
+else
+    echo "done"
+fi
--- helpers/test.sh
+++ helpers/test.sh
@@ -1,69 +1,84 @@
 #!/bin/bash
 
+MACHINE_NAME=verbose-robot
+
 set -e
 
 echo 'giving things a chance to settle...'
-sleep 10
+sleep 30
 
 echo 'testing connectivity'
-curl -v -k https://localhost
-sudo -E -u cif cif -d -p
+docker exec -it verbose-robot cif -d -p
 
 echo 'testing query'
-sudo -E -u cif cif  --search example.com
+docker exec -it verbose-robot cif --search example.com
 
 echo 'waiting...'
 sleep 5
 
 echo 'testing query'
-sudo -E -u cif cif --search example.com
+docker exec -it verbose-robot cif --search example.com
 
 echo 'waiting...'
 sleep 5
 
-sudo -E -u cif cif --itype ipv4 --tags saerch
+docker exec -it verbose-robot cif --itype ipv4 --tags saerch
 
-sudo -E -u cif cif -q 93.184.216.34
+docker exec -it verbose-robot cif -q 93.184.216.34
 
 echo 'waiting...'
 sleep 5
 
-sudo -E -u cif cif -q 93.184.216.34
+docker exec -it verbose-robot cif -q 93.184.216.34
 
-sudo -E -u cif csirtg-fm -r /etc/cif/rules/default/openphish.yml -d --remember --client cif --limit 100 --skip-invalid
-sudo -E -u cif csirtg-fm -r /etc/cif/rules/default/openphish.yml -d --remember --client cif --config /etc/cif/csirtg-fm.yml --limit 100 --skip-invalid
-sudo -E -u cif csirtg-fm -r /etc/cif/rules/default/csirtg.yml -f darknet -d --remember --client cif --limit 100 --skip-invalid
-sudo -E -u cif csirtg-fm -r /etc/cif/rules/default/csirtg.yml -f uce-urls -d --remember --client cif --limit 100 --skip-invalid
+declare -a CMDS=(
+    "-r /etc/cif/rules/default/openphish.yml -d --client cif --limit 100 --skip-invalid"
+    "-r /etc/cif/rules/default/openphish.yml -d --client cif --limit 100 --skip-invalid"
+    "-r /etc/cif/rules/default/csirtg.yml -f darknet -d --remember --client cif --limit 100 --skip-invalid"
+    "-r /etc/cif/rules/default/csirtg.yml -f uce-urls -d --remember --client cif --limit 100 --skip-invalid"
+)
+
+for i in "${CMDS[@]}"; do
+    echo "$i"
+    docker exec -it ${MACHINE_NAME} csirtg-fm ${i}
+done
 
 echo 'waiting 30s... let hunter do their thing...'
 sleep 30
 
-sudo -E -u cif cif --provider csirtg.io
+declare -a CMDS=(
+    "--provider csirtg.io --no-feed --itype url"
+    "--provider openphish.com --itype url"
+    "--itype ipv4 --tags scanner"
+    "--itype ipv4 --tags scanner --days 17"
+    "--itype fqdn --tags search"
+    "--itype url --tags uce"
+    "--itype url --tags phishing"
+    "--itype ipv4 --tags phishing --confidence 2"
+    "--itype ipv4 --confidence 1,4 --no-feed -d"
+    "--itype fqdn --confidence 1,4 --no-feed -d"
+    "--itype fqdn --probability 68,99 --no-feed -d"
+)
 
-sudo -E -u cif cif --provider openphish.com
+for i in "${CMDS[@]}"; do
+    echo "$i"
+    docker exec -it ${MACHINE_NAME} cif ${i}
+done
 
-sudo -E -u cif cif --itype ipv4 --feed --tags scanner
-
-sudo -E -u cif cif --itype ipv4 --feed --tags scanner --days 17
-
-sudo -E -u cif cif --itype fqdn --feed --tags search
-
-sudo -E -u cif cif --itype url --feed --tags uce
-
-sudo -E -u cif cif --itype url --feed --tags phishing
-
-sudo -E -u cif cif --itype ipv4 --feed --tags phishing --confidence 2
-
-sudo -E -u cif cif --itype ipv4 --confidence 1,6 --no-feed -d
-
-sudo -E -u cif cif --itype fqdn --confidence 1,6 --no-feed -d
 
 echo
 echo
 echo "testing tokens"
 
-sudo -E -u cif cif-tokens
-sudo -E -u cif cif-tokens --user test-write --write --create
-sudo -E -u cif cif-tokens --user test-read --read --create
-sudo -E -u cif cif-tokens --user test-read-write --write --create --read
-sudo -E -u cif CIFSDK_CLIENT_HTTP_TRACE=1 cif-tokens -d
+declare -a CMDS=(
+    "-d"
+    "--user test-write --write --create"
+    "--user test-read --read --create"
+    "--user test-read-write --write --create --read"
+    "-d"
+)
+
+for i in "${CMDS[@]}"; do
+    echo "$i"
+    docker exec -e CIFSDK_CLIENT_HTTP_TRACE=1 -it ${MACHINE_NAME} cif-tokens ${i}
+done
