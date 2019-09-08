a2f1ca3c2a35413eb3d119d8d2082517aa2b6816
Wes Young <wes@barely3am.com>
2019-09-08 10:43:22 -0400

    release tweaks

--- docker/run_test_es.sh
+++ docker/run_test_es.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DOCKER_CONFIGS="$(pwd)"
+
 if [[ "${CIF_TOKEN}" == "" ]]; then
     CIF_TOKEN=`head -n 25000 /dev/urandom | openssl dgst -sha256`
     echo "CIF_TOKEN Generated"
@@ -13,7 +15,7 @@ echo ""
 echo "${CIF_TOKEN}"
 echo ""
 
-C=$(docker run --rm -e CSIRTG_TOKEN="${CSIRTG_TOKEN}" -e CIF_STORE_STORE="elasticsearch" -e CIF_ES_NODES="${CIF_ES_NODES}" -e CIF_TOKEN="${CIF_TOKEN}" -e CIF_HUNTER_ADVANCED=1 -e CIF_HUNTER_THREADS=2 -e CIF_HUNTER_TRACE=1 -it -d -p 5000:5000 --name verbose-robot --memory 2g --memory-swap 4g csirtgadgets/verbose-robot)
+C=$(docker run --rm -e CSIRTG_TOKEN="${CSIRTG_TOKEN}" -e CIF_STORE_STORE="elasticsearch" -e CIF_ES_NODES="${CIF_ES_NODES}" -e CIF_TOKEN="${CIF_TOKEN}" -e CIF_HUNTER_ADVANCED=1 -e CIF_HUNTER_THREADS=2 -e CIF_HUNTER_TRACE=1 -it -d -p 5000:5000 --name verbose-robot --memory 2g --memory-swap 4g -v "${DOCKER_CONFIGS}"/geo/:/var/lib/GeoIP/ csirtgadgets/verbose-robot)
 
 bash docker/test.sh
 
